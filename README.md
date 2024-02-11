06af7cf4bac36de35bf4e3b8c5b8d5e86c5af7ed
ChoiSungwook <sungwook0724@lguplus.co.kr>
2024-02-11 11:24:14 +0900

    Add net/http helloworld example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# admission_controller_practice
+# 개요
 kubernetes adminssion controller 연습
+
+# 실행 방법
+
+```sh
+go run .
+```
+
+# 참고자료
+* https://github.com/GoesToEleven/golang-web-dev
+* https://koras02.tistory.com/236
--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+type requestAndResponse map[string]string
+
+func (m requestAndResponse) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for k, v := range m {
+		fmt.Fprintf(w, "%s: %s\n", k, v)
+	}
+}
+
 func main() {
-	println("Hello, World!")
+	requestSample := requestAndResponse{
+		"hello": "world",
+		"do":    "you see me?",
+	}
+	http.ListenAndServe(":8080", requestSample)
 }
